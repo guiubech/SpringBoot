0a8dc42b5b3347ddae2d7b5711f8a84eaf8fc4f6
guiubech <guiubech@hotmail.com>
2019-04-04 22:57:32 -0300

    Add files via upload

---
+++ Eventos/src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final static String[] PERMIT_PATH =  {"/resources/**", "/css/**","/fonts/**",
+			"/img/**","/js/**","/favicon.png","/esqueci-minha-senha","/cadastrar", "/recuperaSenha"};
+
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests()
+		.antMatchers(PERMIT_PATH).permitAll().anyRequest().authenticated().and().formLogin()
+		.loginPage("/entrar").defaultSuccessUrl("/home", true).permitAll().and().logout().logoutUrl("/logout").permitAll();
+
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
